e69173b0184b347c228a1def0c3d0ad7cf3fe345
sannarossang <sanna.rossang@gmail.com>
2023-03-20 16:02:01 +0100

    (build): project startup

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  plugins: ['prettier', '@typescript-eslint'],
+  extends: ['standard-with-typescript', 'plugin:prettier/recommended', 'eslint:recommended'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:@typescript-eslint/eslint-recommended',
+      ],
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    project: true,
+    sourceType: 'module',
+  },
+  parser: '@typescript-eslint/parser',
+}
